80385c4b16eb99b94cf4e42731478349ee95b76d
kymaka <seafth345@gmail.com>
2022-08-29 23:27:24 +0700

    Added task 1 from homework 1.

---
+++ hm_1/task_1/src/main/java/Main.java
@@ -0,0 +1,52 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+
+  // For random number generator.
+  int seed = 123;
+
+  /**
+   * Used for generating random number.
+   * Generates same sequences for same seed number. *
+   * @return int number.
+   */
+  int rand() {
+    seed ^= seed << 13;
+
+    seed ^= seed >> 17;
+
+    seed ^= seed << 5;
+
+    return seed / 10000;
+  }
+
+  public static void main(String[] args) {
+    Main m = new Main();
+    int[][] arr = new int[6][6];
+    // Populating 2d array with random numbers.
+    for (int row = 0; row < arr.length; row++)
+      for (int col = 0; col < arr[row].length; col++)
+        arr[row][col] = m.rand();
+
+    int min = 10000000;
+    int max = 0;
+    int sum = 0;
+    // Calculating sum of all elements and finding minimum and maximum.
+    for (int[] row : arr)
+      for (int el : row) {
+        if (el < min)
+          min = el;
+        if (el > max)
+          max = el;
+        sum += el;
+      }
+
+    System.out.println("Minimum: " + min);
+    System.out.println("Maximum: " + max);
+    // Calculates and prints average.
+    System.out.println("Average: " + sum / (arr.length * arr.length));
+  }
+}
